d328831998fbcf846e5da1afbff1c0f9a88a01a4
murphy-mi <murphymax1@gmail.com>
2022-06-23 12:04:31 -0400

    ArtistPage artists POST fetch

--- src/components/ArtistPage.js
+++ src/components/ArtistPage.js
@@ -14,6 +14,28 @@ function ArtistPage({ spotify, username }) {
   const [showAllTime, setShowAllTime] = useState(false);
 
   useEffect(() => {
+    const artistNames = topArtists.map(artist => artist.name)
+    fetch("http://localhost:3000/users")
+      .then(res => res.json())
+      .then(users => {
+        const user = users.find(user => user.name === username)
+        const body = {
+          user_id: user.id,
+          artistlist: artistNames
+        }
+        fetch(`http://localhost:3000/top_artistlists`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(body)
+        })
+          .then(res => res.json())
+          .then(console.log)
+      })
+  }, [topArtists])
+
+  useEffect(() => {
     const timeRange = showAllTime ? "long_term" : "medium_term";
     spotify
       .getMyTopArtists({ limit: 50, time_range: timeRange })
